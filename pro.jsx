ed8a7ef3c369eb1aefb20f95b98f926432df6562
goldsun27 <todayhonest27@gmail.com>
2023-04-25 23:19:51 +0200

    update code for calc price

--- pro.jsx
+++ pro.jsx
@@ -3,19 +3,37 @@ let age = 5; // example age in years
 let weight = 1200; // example weight in pounds
 let health_status = "Good"; // example health status
 
+// determine market price
 let breed_market_price_per_pound;
-if (breed === "Angus") {
-  breed_market_price_per_pound = 2.8; // Angus market price per pound
-} else if (breed === "Hereford") {
-  breed_market_price_per_pound = 2.4; // Hereford market price per pound
-} else if (breed === "Charolais") {
-  breed_market_price_per_pound = 2.6; // Charolais market price per pound
+if (
+  ["Angus", "Hereford", "Charolais", "Limousin", "Simmental"].includes(breed)
+) {
+  // top US breed
+  switch (breed) {
+    case "Angus":
+      breed_market_price_per_pound = 2.8;
+      break;
+    case "Hereford":
+      breed_market_price_per_pound = 2.4;
+      break;
+    case "Charolais":
+      breed_market_price_per_pound = 2.6;
+      break;
+    case "Limousin":
+      breed_market_price_per_pound = 2.7;
+      break;
+    case "Simmental":
+      breed_market_price_per_pound = 2.5;
+      break;
+  }
 } else {
-  breed_market_price_per_pound = 2.2; // Average market price per pound for non-top breeds
+  // average market price per pound for non-top breeds
+  breed_market_price_per_pound = 2.2;
 }
 
 let market_value = weight * breed_market_price_per_pound;
 
+// determine adjustment factors
 let age_adjustment_factor;
 if (age < 2) {
   age_adjustment_factor = 1.2; // price adjustment for younger age
@@ -28,14 +46,18 @@ if (age < 2) {
 }
 
 let health_adjustment_factor;
-if (health_status === "Good") {
-  health_adjustment_factor = 1.2; // price adjustment for good health status
-} else if (health_status === "Average") {
-  health_adjustment_factor = 1.0;
-} else if (health_status === "Poor") {
-  health_adjustment_factor = 0.8; // price adjustment for poor health status
-} else {
-  health_adjustment_factor = 0.5; // price adjustment for very poor health status
+switch (health_status) {
+  case "Good":
+    health_adjustment_factor = 1.2; // price adjustment for good health status
+    break;
+  case "Average":
+    health_adjustment_factor = 1.0;
+    break;
+  case "Poor":
+    health_adjustment_factor = 0.8; // price adjustment for poor health status
+    break;
+  default:
+    health_adjustment_factor = 0.5; // price adjustment for very poor health status
 }
 
 let adjusted_value =
